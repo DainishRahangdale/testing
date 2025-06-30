a570a6b8b62ad6201a9f71b2ebabc0423d937891
Dainish Rahangdale <dainish74892002@gmail.com>
2025-07-01 01:09:59 +0530

    env add

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const  users = require('./data.js')
 const jwt = require('jsonwebtoken');
@@ -11,7 +12,7 @@ app.use(express.json());
 
 app.use(cors())
 
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.post('/signup',(req, res)=>{
     
